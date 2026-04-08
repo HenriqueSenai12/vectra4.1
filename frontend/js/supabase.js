8a61427ed2594ce03fdef21e9e6a3deca24a3cef
HenriqueSenai12 <henrique.l.silva8@aluno.senai.br>
2026-04-08 02:57:48 -0300

    update

--- frontend/js/supabase.js
+++
@@ -1,8 +0,0 @@
-// Importa o SDK do Supabase direto da web (CDN)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://ncdviiijzbbqugyiusyq.supabase.co'
-const supabaseKey = 'sb_publishable_Lwh8-C2Ah3PLP-riPKtq8w_R4oxJgxC'
-
-// Exporta a conexão para usar em outros arquivos
-export const supabase = createClient(supabaseUrl, supabaseKey)
