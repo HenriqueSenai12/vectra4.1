37f63f0306969e7f0facaf1e955faf213323140a
HenriqueSenai12 <henrique.l.silva8@aluno.senai.br>
2026-04-09 10:03:07 -0300

    update monitoramento.js

--- frontend/js/monitoramento.js
+++ frontend/js/monitoramento.js
@@ -57,11 +57,18 @@ document.addEventListener("DOMContentLoaded", () => {
                 stroke: { curve: 'smooth', width: 3 },
                 fill: { type: 'gradient', gradient: { shadeIntensity: 1, opacityFrom: 0.4, opacityTo: 0.05, stops: [0, 100] } },
                 xaxis: { categories: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'], labels: { style: { colors: themeColors.text } } },
-                yaxis: { labels: { style: { colors: themeColors.text } } },
+                
+                // 👇 AQUI ESTÁ A CORREÇÃO (Adicionamos a propriedade 'max')
+                yaxis: { 
+                    labels: { style: { colors: themeColors.text } },
+                    max: (max) => { return Math.ceil(max * 1.2); } // Dá 20% de margem no topo do gráfico
+                },
+                
                 legend: { position: 'top', horizontalAlign: 'right', labels: { colors: '#fff' } }
             });
             chartLine.render();
         }
+
 
         // Gráfico 2: Donut
         if (document.querySelector("#donut-chart")) {
